4753e1802aeaf0e0ad27b4f14714f3746abaafc6
RoyNikyee <2102141297@qq.com>
2019-11-18 17:46:52 +0800

    数据库连接成功

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,15 +1,21 @@
 <?php
 namespace app\index\controller;
-
+use think\Controller;
 use think\Db;
-class Index
+class Index extends Controller
 {
     public function index()
     {
-        //从数据库中读取数据　　　
-        $data=Db::table('student')->select();
-        // 2、输出查询SQL字符串
-        return $data;
+//        //从数据库中读取数据　　　
+//        $data=Db::table('student')->select();
+//        // 2、输出查询SQL字符串
+//        return $data;
+        //dump(config('database'));
+        //$res = Db::connect();
+        //dump($res);
+        $res = Db::query("select * from student where id=?",[1]);
+        dump($res);
+
 
     }
 }
